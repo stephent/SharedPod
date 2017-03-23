b89ef79d0d330a3df4d986bbe025892ec06c1c3e
Stephen Trainor <stephen@crookneckconsulting.com>
2017-03-23 08:59:58 -0600

    Initial commit - empty framework and empty project

---
+++ MyFramework/MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Stephen Trainor on 3/23/17.
+//  Copyright © 2017 Crookneck Consulting LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
